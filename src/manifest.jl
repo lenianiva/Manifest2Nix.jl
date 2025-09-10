5babafedcb4add86bc70cc47b09928372105f793
Leni Aniva <a@leni.sh>
2025-09-10 00:56:27 -0700

    feat: Emit dependencies

--- src/manifest.jl
+++ src/manifest.jl
@@ -7,18 +7,24 @@ using Base: UUID, @kwdef
 
 @kwdef struct PinnedManifest
     uuid::UUID
+    dependencies::Vector{String}
     is_tracking_registry::Bool
 end
 
 function to_pinned_manifest(uuid::UUID, info::PackageInfo)::PinnedManifest
     return PinnedManifest(
         uuid=uuid,
-        is_tracking_registry=info.is_tracking_registry
+        is_tracking_registry=info.is_tracking_registry,
+        dependencies=collect(keys(info.dependencies)),
     )
 end
 
 format(nothing) = "null"
-format(x::PinnedManifest) = Dict("uuid" => x.uuid, "is_tracking_registry" => x.is_tracking_registry)
+format(x::PinnedManifest) = Dict(
+    "uuid" => x.uuid,
+    "is_tracking_registry" => x.is_tracking_registry,
+    "dependencies" => x.dependencies
+)
 format(u::UUID) = string(u)
 
 function load_dependencies(; path_output::Union{Some{String}, Nothing}=nothing)
