e637d706fe38f172a511b8b37aa9e062a12a06f9
Leni Aniva <a@leni.sh>
2025-09-09 22:38:34 -0700

    feat: Add parser arguments

--- src/manifest.jl
+++ src/manifest.jl
@@ -1,5 +1,6 @@
 import Pkg
 import TOML
+import ArgParse
 using Pkg.API: PackageInfo
 using Base: UUID, @kwdef
 
@@ -15,10 +16,11 @@ function to_pinned_manifest(uuid::UUID, info::PackageInfo)::PinnedManifest
     )
 end
 
+format(nothing) = "null"
 format(x::PinnedManifest) = Dict("uuid" => x.uuid, "is_tracking_registry" => x.is_tracking_registry)
 format(u::UUID) = string(u)
 
-function load_dependencies(path_output::Union{Some{String}, Nothing}=nothing)
+function load_dependencies(; path_output::Union{Some{String}, Nothing}=nothing)
     dependencies = Pkg.dependencies()
     payload = Dict(
         v.name => to_pinned_manifest(k, v)
@@ -39,4 +41,23 @@ function load_dependencies(path_output::Union{Some{String}, Nothing}=nothing)
     end
 end
 
-load_dependencies()
+if abspath(PROGRAM_FILE) == @__FILE__
+    settings = ArgParse.ArgParseSettings()
+    @ArgParse.add_arg_table! settings begin
+        "-p" "--project"
+        help = "Root to project"
+        required = false
+        arg_type = String
+        default = pwd()
+        "-o", "--output"
+        help = "Output manifest file"
+        required = false
+        arg_type = String
+        default = nothing
+    end
+    config = ArgParse.parse_args(settings)
+
+    Pkg.Operations.with_temp_env(config["project"]) do
+        load_dependencies(path_output=get(config, "output", nothing))
+    end
+end
