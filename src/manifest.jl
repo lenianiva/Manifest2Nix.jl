73891cde30f9350156495e1031f468f9d1473680
Leni Aniva <a@leni.sh>
2025-09-09 21:48:34 -0700

    feat: Conditional output to file

--- src/manifest.jl
+++ src/manifest.jl
@@ -1,16 +1,42 @@
 import Pkg
+import TOML
 using Pkg.API: PackageInfo
-using Base: UUID
+using Base: UUID, @kwdef
 
-function to_pinned_manifest(id::UUID, info::PackageInfo)::Dict
-    return Dict()
+@kwdef struct PinnedManifest
+    uuid::UUID
+    is_tracking_registry::Bool
 end
 
-function load_dependencies(output_path::String)
+function to_pinned_manifest(uuid::UUID, info::PackageInfo)::PinnedManifest
+    return PinnedManifest(
+        uuid=uuid,
+        is_tracking_registry=info.is_tracking_registry
+    )
+end
+
+format(x::PinnedManifest) = Dict("uuid" => x.uuid, "is_tracking_registry" => x.is_tracking_registry)
+format(u::UUID) = string(u)
+
+function load_dependencies(path_output::Union{Some{String}, Nothing}=nothing)
     dependencies = Pkg.dependencies()
-    for (k, v) in dependencies
-        println("$(v.name): [$k] $(v.git_source)@$(v.git_revision)")
+    payload = Dict(
+        v.name => to_pinned_manifest(k, v)
+        for (k, v) in dependencies
+    )
+    project_info = Pkg.project()
+    result = Dict(
+        "name" => project_info.name,
+        "uuid" => project_info.uuid,
+        "deps" => payload,
+    )
+
+    writer = io::IO -> TOML.print(format, io, result)
+    if isnothing(path_output)
+        writer(stdout)
+    else
+        open(writer, path_output, "w")
     end
 end
 
-load_dependencies("hi")
+load_dependencies()
