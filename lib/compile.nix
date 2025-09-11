01ea0f800beef2d5c30390944ac89e0a50d92189
Leni Aniva <a@leni.sh>
2025-09-10 22:47:11 -0700

    fix: Force offline mode and add certificates

--- lib/compile.nix
+++ lib/compile.nix
@@ -3,22 +3,29 @@
   lib,
   pkgs,
   julia ? pkgs.julia-bin,
+  stdenv,
+  cacert,
   ...
-}: {
+}: let
+  abridged-version = "${lib.versions.major julia.version}.${lib.versions.minor julia.version}";
+in {
   buildJuliaPackage = args @ {src, ...}: let
     project = builtins.fromTOML (builtins.readFile "${src}/Project.toml");
   in
-    pkgs.stdenv.mkDerivation (args
+    stdenv.mkDerivation (args
       // {
         inherit (project) name version;
         nativeBuildInputs = [julia];
         JULIA_PKG_OFFLINE = "true";
+        JULIA_PKG_SERVER = "";
         JULIA_DEPOT_PATH = ".julia";
+        JULIA_SSL_CA_ROOTS_PATH = cacert;
         buildPhase = ''
-          mkdir .julia
+          mkdir -p $JULIA_DEPOT_PATH
+          julia --project -e "import Pkg; Pkg.Registry.rm(\"General\"); Pkg.build()"
+
           mkdir -p $out
-          julia --project -e "import Pkg; Pkg.precompile()"
-          cp -r $JULIA_DEPOT_PATH/compiled/${julia.version} $out/
+          cp -r $JULIA_DEPOT_PATH/compiled/v${abridged-version} $out/
         '';
       });
 }
