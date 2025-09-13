b763d7e24f2847fc72af84c626a843c7ab57f035
Leni Aniva <a@leni.sh>
2025-09-12 22:25:26 -0700

    feat: Speed-up by precompiling dependencies

--- lib/compile.nix
+++ lib/compile.nix
@@ -23,28 +23,10 @@ in rec {
       mkdir -p $out
       JULIA_DEPOT_PATH=$out julia --project -e "import Pkg"
     '';
-  # `deps` here must be a list
-  mkDepsDepot = deps: let
-    packages = symlinkJoin {
-      name = "packages";
-      paths = builtins.map (dep:
-        stdenv.mkDerivation {
-          name = "${dep}-src";
-          inherit (dep) src JULIA_DEPOT_PATH;
-          phases = ["installPhase"];
-          # FIXME: Individualize this hash
-          installPhase = ''
-            mkdir -p $out/${dep.name}
-            ln -s ${dep.src} $out/${dep.name}/kML3d
-          '';
-        })
-      deps;
-    };
-  in
+  mkDepsDepot = deps:
     stdenv.mkDerivation
     {
       name = "deps";
-      nativeBuildInputs = [packages];
       src = symlinkJoin {
         name = "deps";
         paths = deps;
@@ -53,7 +35,6 @@ in rec {
       installPhase = ''
         mkdir -p $out/compiled/
         ln -s $src/ $out/compiled/v${abridged-version}
-        ln -s ${packages} $out/packages
       '';
     };
   buildJuliaPackage = args @ {
@@ -65,8 +46,10 @@ in rec {
     ...
   }: let
     project = builtins.fromTOML (builtins.readFile "${src}/Project.toml");
-    load-paths = builtins.map (dep: "${dep.src}") deps;
-    load-path = lib.concatStringsSep ":" load-paths;
+    load-path = symlinkJoin {
+      name = "deps";
+      paths = builtins.map (dep: dep.load-path) deps;
+    };
     deps-depot =
       if deps == null
       then []
@@ -75,16 +58,15 @@ in rec {
   in
     stdenv.mkDerivation (args
       // {
-        deps-depot = mkDepsDepot deps;
         inherit (project) name version;
-        nativeBuildInputs = [julia deps-depot] ++ depots;
+        nativeBuildInputs = [julia];
         JULIA_LOAD_PATH = "${load-path}:";
         JULIA_DEPOT_PATH = ".julia:${input-depots}";
         JULIA_SSL_CA_ROOTS_PATH = cacert;
         buildPhase = ''
           mkdir -p .julia
-          ls ${stdlib-depot}/compiled/v1.11
-          ls ${mkDepsDepot deps}/compiled/v1.11
+          mkdir -p .julia/packages/TextWrap/
+          mkdir -p .julia/packages/ArgParse/
 
           if [ ! -f Manifest.toml ]; then
             ln -s ${lib.defaultTo "none" parent-manifest} Manifest.toml
@@ -95,6 +77,16 @@ in rec {
           mkdir -p $out
           cp -r .julia/compiled/v${abridged-version}/* $out/
         '';
+        # A special derivation for creating load paths
+        load-path = stdenv.mkDerivation rec {
+          inherit (project) name;
+          inherit src;
+          phases = ["unpackPhase" "installPhase"];
+          installPhase = ''
+            mkdir $out
+            cp -r ${src} $out/${name}
+          '';
+        };
       });
   # Create a Julia package from a dependency file
   buildJuliaPackageWithDeps = args @ {
@@ -103,6 +95,7 @@ in rec {
     ...
   }: let
     lock = builtins.fromTOML (builtins.readFile lockFile);
+
     depToPackage = name: dep @ {
       repo,
       rev,
@@ -123,6 +116,7 @@ in rec {
         # FIXME: Handle different parent manfiest
         parent-manifest = "${src}/Manifest.toml";
       };
+
     allDeps = builtins.mapAttrs depToPackage lock.deps;
   in
     buildJuliaPackage (args
