c19df2bdf59749aaa8de05d71647f050672ecef8
Leni Aniva <a@leni.sh>
2025-10-09 16:34:03 -0700

    doc: Flake for minimal project

---
+++ templates/minimal/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Minimal Julia Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    manifest2nix.url = "git+https://codeberg.org/aniva/Manifest2Nix.jl.git";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    manifest2nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {system, ...}: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(manifest2nix.fromManifest ./Manifest.toml)];
+        };
+
+        inherit (pkgs) julia;
+        m2nlib = pkgs.callPackage manifest2nix.mkLib {};
+        package = m2nlib.buildJuliaPackage {src = ./.;};
+      in {
+        packages = rec {
+          default =
+            pkgs.runCommand "mystery"
+            (m2nlib.createPackageEnv package)
+            ''
+              ${julia}/bin/julia -e "import Minimal; Minimal.mystery();" > $out
+            '';
+        };
+
+        devShells.default =
+          pkgs.mkShell {
+          };
+      };
+    };
+}
