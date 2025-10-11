e72e038d047d442578f79c7a81698a7a8dad2382
Leni Aniva <a@leni.sh>
2025-10-11 12:20:35 -0700

    doc: Add flake for simple template

---
+++ templates/simple/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Simple Julia Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    manifest2nix.url = "git+https://codeberg.org/aniva/Manifest2Nix.jl.git";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    manifest2nix,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {system, ...}: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(manifest2nix.fromManifest ./Manifest.toml)];
+        };
+
+        inherit (pkgs) julia;
+        m2nlib = manifest2nix.mkLib pkgs;
+        package = m2nlib.buildJuliaPackageWithDeps {src = ./.;};
+      in {
+        packages = rec {
+          default =
+            pkgs.runCommand "mystery"
+            (m2nlib.createPackageEnv package)
+            ''
+              ${julia}/bin/julia -e "println(1)" > $out
+            '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [julia m2nlib.manifest2nix];
+        };
+      };
+    };
+}
